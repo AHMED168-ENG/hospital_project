d4f2d48564954ac44d3e36f5c388d7bda0b04056
ahmed reda 2 <88160434+ahmed123reda@users.noreply.github.com>
2022-06-27 21:40:16 +0200

    2 update

--- config/config.js
+++ config/config.js
@@ -1,14 +1,16 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: "01024756410ahmed",
     database: process.env.DB,
     host: process.env.DB_HOST,
     dialect: "postgres",
   },
   production: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: "01024756410ahmed",
     database: process.env.DB,
     host: process.env.DB_HOST,
     dialect: "postgres",
